108ce1ee36a307eb428055c747a90142d1a6b032
Darragh Enright <darraghenright@gmail.com>
2020-01-16 09:49:37 +0000

    added tests and Todo.id

--- lib/todos/todo.ex
+++ lib/todos/todo.ex
@@ -1,11 +1,13 @@
 defmodule Todos.Todo do
   alias Todos.Todo
 
-  @enforce_keys [:text]
-  defstruct text: nil, complete?: false
+  @enforce_keys [:id, :text]
+  defstruct id: nil,
+            text: nil,
+            complete?: false
 
   def new(text) do
-    %Todo{text: text}
+    %Todo{id: UUID.uuid4(), text: text}
   end
 
   def complete(%Todo{} = todo) do
--- test/todos/todo_test.exs
+++ test/todos/todo_test.exs
@@ -7,8 +7,16 @@ defmodule Todos.TodoTest do
     %{todo: Todo.new("Eat dinner")}
   end
 
+  test "a new todo is assigned an id", %{todo: todo} do
+    refute nil == todo.id
+  end
+
   test "a new todo has a text field", %{todo: todo} do
     assert "Eat dinner" == todo.text
+  end
+
+  test "a todo constructor must specify a text field" do
+    assert_raise UndefinedFunctionError, fn -> Todo.new() end
   end
 
   test "a new todo is not complete", %{todo: todo} do
@@ -16,7 +24,6 @@ defmodule Todos.TodoTest do
   end
 
   test "a todo can be marked complete", %{todo: todo} do
-    todo = Todo.complete(todo)
-    assert true == todo.complete?
+    assert true == Todo.complete(todo).complete?
   end
 end
