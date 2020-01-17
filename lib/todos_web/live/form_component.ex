0afdf1e32883133718867300742eee6b57c5fb84
Darragh Enright <darraghenright@gmail.com>
2020-01-17 10:38:44 +0000

    adding new todods

--- lib/todos_web/live/form_component.ex
+++ lib/todos_web/live/form_component.ex
@@ -1,10 +1,16 @@
 defmodule TodosWeb.FormComponent do
   use Phoenix.LiveComponent
 
+  alias Phoenix.LiveView.Socket
   alias Phoenix.View
   alias TodosWeb.TodoView
 
   def render(assigns) do
-    View.render(TodoView, "form_component.html", assigns)
+    View.render TodoView, "form_component.html", assigns
+  end
+
+  def handle_event("new", %{"text" => text}, %Socket{} = socket) do
+    send(self(), {:new, text})
+    {:noreply, socket}
   end
 end
---
+++ lib/todos_web/live/todo_live.ex
@@ -0,0 +1,21 @@
+defmodule TodosWeb.TodoLive do
+  use Phoenix.LiveView
+
+  alias Phoenix.LiveView.Socket
+  alias Phoenix.View
+  alias Todos.{Todo, TodoList}
+  alias TodosWeb.TodoView
+
+  def mount(%{todo_list: todo_list}, %Socket{} = socket) do
+    {:ok, assign(socket, :todo_list, todo_list)}
+  end
+
+  def render(assigns) do
+    View.render(TodoView, "todo.html", assigns)
+  end
+
+  def handle_info({:new, text}, %Socket{assigns: %{todo_list: todo_list}} = socket) do
+    todo_list = TodoList.add(todo_list, Todo.new(text))
+    {:noreply, assign(socket, :todo_list, todo_list)}
+  end
+end
